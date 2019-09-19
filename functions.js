70dc4f75a744779fe6f5e018865facab004e8bc5
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-09-19 18:15:57 +0200

    Initial version of my awsome supermarket app

---
+++ functions.js
@@ -0,0 +1,9 @@
+console.log("Hello from the functions.js file")
+
+
+const icecream = document.getElementById("product0")
+console.log("icecream", icecream)
+let pricetag = document.createElement('h2')
+pricetag.innerHTML = '2.5'
+console.log("pricetag",pricetag)
+icecream.appendChild(pricetag)
