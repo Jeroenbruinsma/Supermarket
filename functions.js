dcd10b4822969b63da630119d7f84a19f5f5b4e6
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-09-19 18:17:28 +0200

    Make the code look nice

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 console.log("Hello from the functions.js file")
 
-
 const icecream = document.getElementById("product0")
 console.log("icecream", icecream)
 let pricetag = document.createElement('h2')
